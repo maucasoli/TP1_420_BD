4bd3cf8096b17f5eaff5688773c13a3fcb813786
Mauricio Oliveira <mauriciocasoli@gmail.com>
2025-10-14 11:31:28 -0400

    Added clients panel and connectionString in .env file.

--- TP1_420_BD/Form1.Designer.cs
+++ TP1_420_BD/Form1.Designer.cs
@@ -32,7 +32,12 @@
             title = new Label();
             clientsButton = new Button();
             commandesButton = new Button();
+            Clients = new Panel();
+            dgvClients = new DataGridView();
+            label1 = new Label();
             Home.SuspendLayout();
+            Clients.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvClients).BeginInit();
             SuspendLayout();
             // 
             // Home
@@ -50,10 +55,10 @@
             // 
             title.AutoSize = true;
             title.BackColor = Color.Transparent;
-            title.Font = new Font("Montserrat", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            title.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             title.Location = new Point(424, 109);
             title.Name = "title";
-            title.Size = new Size(354, 33);
+            title.Size = new Size(336, 25);
             title.TabIndex = 2;
             title.Text = "Gestion Clients et Commandes";
             title.TextAlign = ContentAlignment.TopRight;
@@ -67,7 +72,7 @@
             clientsButton.FlatAppearance.MouseDownBackColor = Color.Gainsboro;
             clientsButton.FlatAppearance.MouseOverBackColor = Color.WhiteSmoke;
             clientsButton.FlatStyle = FlatStyle.Flat;
-            clientsButton.Font = new Font("Montserrat", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            clientsButton.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             clientsButton.ForeColor = Color.DimGray;
             clientsButton.Location = new Point(523, 183);
             clientsButton.Name = "clientsButton";
@@ -75,6 +80,7 @@
             clientsButton.TabIndex = 0;
             clientsButton.Text = "Clients";
             clientsButton.UseVisualStyleBackColor = false;
+            clientsButton.Click += clientsButton_Click;
             // 
             // commandesButton
             // 
@@ -85,7 +91,7 @@
             commandesButton.FlatAppearance.MouseDownBackColor = Color.PapayaWhip;
             commandesButton.FlatAppearance.MouseOverBackColor = Color.OldLace;
             commandesButton.FlatStyle = FlatStyle.Flat;
-            commandesButton.Font = new Font("Montserrat", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            commandesButton.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             commandesButton.ForeColor = Color.DarkGoldenrod;
             commandesButton.Location = new Point(523, 255);
             commandesButton.Name = "commandesButton";
@@ -94,17 +100,52 @@
             commandesButton.Text = "Commandes";
             commandesButton.UseVisualStyleBackColor = false;
             // 
+            // Clients
+            // 
+            Clients.BackgroundImage = Properties.Resources.bg_tp1;
+            Clients.Controls.Add(dgvClients);
+            Clients.Controls.Add(label1);
+            Clients.Location = new Point(0, -1);
+            Clients.Name = "Clients";
+            Clients.Size = new Size(801, 453);
+            Clients.TabIndex = 2;
+            Clients.Visible = false;
+            // 
+            // dgvClients
+            // 
+            dgvClients.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvClients.Location = new Point(107, 101);
+            dgvClients.Name = "dgvClients";
+            dgvClients.Size = new Size(613, 272);
+            dgvClients.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(241, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(336, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Gestion Clients et Commandes";
+            label1.TextAlign = ContentAlignment.TopRight;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(Clients);
             Controls.Add(Home);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
             Text = "Gestion Clients et Commandes";
             Home.ResumeLayout(false);
             Home.PerformLayout();
+            Clients.ResumeLayout(false);
+            Clients.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvClients).EndInit();
             ResumeLayout(false);
         }
 
@@ -114,5 +155,8 @@
         private Button clientsButton;
         private Button commandesButton;
         private Label title;
+        private Panel Clients;
+        private DataGridView dgvClients;
+        private Label label1;
     }
 }
--- TP1_420_BD/Form1.cs
+++ TP1_420_BD/Form1.cs
@@ -1,10 +1,58 @@
+using System.Data;
+using DotNetEnv;
+using Microsoft.Data.SqlClient;
+
 namespace TP1_420_BD
 {
     public partial class Form1 : Form
     {
+        string server;
+        string database;
+        string conStr;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Récupérer les variables depuis le fichier .env
+            Env.Load();
+            server = Env.GetString("SERVER");
+            database = Env.GetString("DATABASE");
+            conStr = $"Server={server};Database={database};Trusted_Connection=True;TrustServerCertificate=True";
+
+        }
+
+        //private void clientsButton_Click(object sender, EventArgs e)
+        //{
+        //    Home.Visible = false;
+        //    Clients.Visible = true;
+        //    Clients.BringToFront();
+
+        //    ReadTable();
+        //}
+
+        private void ReadTable()
+        {
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+
+                string select = "SELECT * FROM Clients;";
+                SqlDataAdapter adapter = new SqlDataAdapter(select, con);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet, "Clients");
+
+                dgvClients.DataSource = dataSet.Tables["Clients"];
+            }
+        }
+
+        private void clientsButton_Click(object sender, EventArgs e)
+        {
+            Home.Visible = false;
+            Clients.Visible = true;
+            Clients.BringToFront();
+
+            ReadTable();
         }
     }
 }
